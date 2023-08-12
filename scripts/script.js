bd70d86daee60bbddc2cc232544c01126a344b8c
JoshuaCDean <u684188640@byui.edu>
2023-08-12 15:04:06 -0500

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -7,23 +7,19 @@ function toggleMenu() {
 
 function checkToggleOff(tar)
 {
-    if ((document.getElementById("hamburgerNav").classList.contains("open") ) && (tar != "navCheck"))
+    if ((document.getElementById("hamburgerNav").classList.contains("open") ) && (tar != "navCheck") && (tar !="hamburgerBtn"))
     {
+        console.log("Shutting Nav Toggle")
         toggleMenu();
     }
 }
-
-document.addEventListener('click', function(e) {
-    tar = e.target.id;
-    checkToggleOff(tar)
-});
 
 const x = document.getElementById("hamburgerBtn");
 
 x.onclick = toggleMenu;
 
 
-let slideIndex = 1;
+/*let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -50,4 +46,43 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}*/
+
+function openTab(target, tab)
+{
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("tabActive", "");
+  }
+
+  document.getElementById(tab).style.display = "block";
+  target.target.classList.toggle("tabActive");
 }
+
+document.addEventListener('mouseover', (e)=>{
+  let tar = e.target.id;
+  if (tar == "hover-image")
+  {
+    if (!document.getElementById("hover-image").classList.contains("hover-blur"))
+    {
+      document.getElementById("hover-image").classList.add("hover-blur")
+    }
+    document.getElementById("hover-info").style.display = "block";
+  }
+});
+
+document.addEventListener('click', function(e) {
+  let tar = e.target.id;
+  checkToggleOff(tar)
+  if (e.target.classList.contains("tablinks"))
+  {
+    openTab(e, e.target.innerText);
+  }
+});
